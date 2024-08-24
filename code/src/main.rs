aa4d20825712db41ad635105a2e6ff40f3b0c2ea
elijah0528 <elijahkurien05@gmail.com>
2024-08-24 00:11:12 -0400

    Add: Raytracer shades sphere (buggy but works)

--- code/src/main.rs
+++ code/src/main.rs
@@ -2,24 +2,30 @@ mod vec3;
 mod color;
 mod ray;
 
-use vec3::Vec3;
-use vec3::Point3;
+use vec3::{Vec3, Point3};
 use color::Color;
 use ray::Ray;
 
-fn hit_sphere(center: Point3, radius: f32, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f32, r: Ray) -> f32 {
     let oc = center - r.origin();
     let a = r.direction().dot(&r.direction());
-    let b = -2.0 * r.direction().dot(&oc);
+    let b = 2.0 * r.direction().dot(&oc);
     let c = oc.dot(&oc) - radius * radius;
 
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt() ) / (2.0 * a)
+    }
 }
 
 fn ray_color (r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0  {
+        let N: Vec3 = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0) * 0.5;
     }
     let unit_direction = r.direction().unit_vector();
     let a = 0.5*(unit_direction.y() + 1.0);
